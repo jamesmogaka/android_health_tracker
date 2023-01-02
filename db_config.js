5d9f4b26c5565d6b7ad7117fa1fce4d443b6fc73
james mogaka <jamesoyondi23@gmail.com>
2023-01-02 18:23:36 +0300

    hidding of the database connection info

--- db_config.js
+++ db_config.js
@@ -1,13 +1,14 @@
 //Pool is a group of client objects that enhances reusability for different database operations
 //importing necessary  libraries
 const Pool = require('pg').Pool;
+require("dotenv").config();
 
 const pool  = new Pool({
-    user:'postgres',
-    password:'5117',
-    host:'localhost',
-    port:5432,
-    database:'health_tracker'
+    user:process.env.DB_USER,
+    password:process.env.DB_PASSWORD,
+    host:process.env.DB_HOST,
+    port:process.env.DB_PORT,
+    database:process.env.DB_DATABASE
 });
 
 
--- messenger.js
+++ messenger.js
@@ -1,65 +1,10 @@
-//Module importation
-const webSocket = require('ws');
-const server = require('./server')
+// importing the necessary modules
+const server = require('./server');
+// importation and creation of a server instance
+const io = require('socket.io')(server);
 
-//
-//Create a web socket server
-const wss = new WebSocket.server({server});
-//
-//Setting up the event listener at connection 
-wss.on('connection', (ws) => {
-  // Set up event handlers for the client socket
-
-  ws.on('message', (message) => {
-    // Handle the message from the client
-    console.log(`{message}`)
-  });
-
-  ws.on('close', () => {
-    // connection closed
-    console.log('connection ended!!')
-  });
-
-});
-
-
-
-/* //how to implement web sockets to create a chat room?
-const rooms = {};
-
-wss.on("connection", socket => {
-  const uuid = ...; // create here a uuid for this connection
-
-  const leave = room => {
-    // not present: do nothing
-    if(! rooms[room][uuid]) return;
-
-    // if the one exiting is the last one, destroy the room
-    if(Object.keys(rooms[room]).length === 1) delete rooms[room];
-    // otherwise simply leave the room
-    else delete rooms[room][uuid];
-  };
-
-  socket.on("message", data => {
-    const { message, meta, room } = data;
-
-    if(meta === "join") {
-      if(! rooms[room]) rooms[room] = {}; // create the room
-      if(! rooms[room][uuid]) rooms[room][uuid] = socket; // join the room
-    }
-    else if(meta === "leave") {
-      leave(room);
-    }
-    else if(! meta) {
-      // send the message to all in the room
-      Object.entries(rooms[room]).forEach(([, sock]) => sock.send({ message }));
-    }
-  });
-
-  socket.on("close", () => {
-    // for each room, remove the closed socket
-    Object.keys(rooms).forEach(room => leave(room));
-  });
-});
- */
+//Event handling of a socket instance
+io.on('connection', socket => {
+  console.log(socket.id);
+})
 
